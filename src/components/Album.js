87e2c9581c3e39a1cc379d3bd075dc8ad2151982
melissa <mycodesite89@gmail.com>
2018-07-12 05:06:13 -0700

    assignment 8:complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -56,10 +56,12 @@ class Album extends Component {
 
 	handleNextClick(){
 		const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
-		const newIndex = Math.max(0, currentIndex + 1);
+		if (currentIndex !== this.state.album.songs.length-1){
+		const newIndex = Math.max(currentIndex + 1);
 		const newSong = this.state.album.songs[newIndex];
 		this.setSong(newSong);
 		this.play();
+	 }
 	}
 	
 	mouseHoverYes(song){
